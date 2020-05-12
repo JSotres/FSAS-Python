65982f17630ace15409683ac0ddd6d8bb7dcbd05
Javier Sotres <jsotres@gmail.com>
2020-05-12 09:09:34 +0200

    Diverse work on ExperimentsTable

--- nanoscopeToSQLite.py
+++ nanoscopeToSQLite.py
@@ -6,6 +6,7 @@ import numpy as np
 import argparse
 import sqlite3
 import io
+import matplotlib.pyplot as plt
 
 ###############################################################################
 # numpy arrays are not supported by sqlite. We need to register them as new
@@ -76,9 +77,11 @@ class NanoscopeForceVolumeFileToDataBase():
         readTopography()
         readFV()
         connectToDataBase()
+        checkTableExists()
         closeDataBaseConnection()
-        createTable()
-        populateFVTable()
+        createTables()
+        populateTables()
+        testFunction()
     '''
 
     def __init__(self, file_name, database_name):
@@ -107,34 +110,12 @@ class NanoscopeForceVolumeFileToDataBase():
 
         # Name of the Force Volume file
         self.file_name = file_name
-
-        # Reads the header of file_name, and populates the
-        # headerParameters dictionary
-        self.readHeader()
-
-        # Finds meaningful parameters from the key values in headerParameters
-        self.headerToParameters()
-
-        # Reads Topography (binary) data from file_name
-        self.readTopography()
-
-        # Reads Force Volume (FV) data from file_name
-        self.readFV()
+        # Name of the Force Volume file to be used in the database
+        # SQLite does not like dots...
+        self.file_name2 = file_name.replace('.', '_')
 
         # Name of the database
         self.database_name = database_name
-
-        # Connects to the database
-        self.connectToDataBase()
-
-        # Creates a table in the database
-        self.createTable()
-
-        # Inserts FV and Tpography data into the table
-        self.populateFVTable()
-
-        # Closes the connection to the database
-        self.closeDataBaseConnection()
 
     def readHeader(self):
         '''
@@ -208,7 +189,7 @@ class NanoscopeForceVolumeFileToDataBase():
     def readTopography(self):
         '''
         Reads (binary) topography data contained in the Force Volume file
-        and (temporally) saves it in the attribute topographyArray 
+        and (temporally) saves it in the attribute topographyArray
         '''
         file = open(self.file_name, 'rb')
         file.seek(int(self.headerParameters['Data offset'][0]))
@@ -261,52 +242,186 @@ class NanoscopeForceVolumeFileToDataBase():
         self.cursor.close()
         self.connector.close()
 
-    def createTable(self):
+    def checkTableExists(self):
         '''
-        Creates a table, FVData, in the database with columns
-        for the Force Volume and topography data
+        Checks if a table with name equalt to that of input
+        force volume name already exists in the database. If so,
+        asks the user if it should proceed. If so,it will drop
+        the already existing table
         '''
-        c = self.connector.cursor()
-        sql_command = """
-        CREATE TABLE IF NOT EXISTS FVData (
+        sql_command = f"""
+        SELECT count(name) FROM sqlite_master
+        WHERE type='table' AND name='{self.file_name2}';
+        """
+        self.cursor.execute(sql_command)
+
+        if self.cursor.fetchone()[0] == 1:
+            string_input = """
+            A table with the file name already exists in the database.
+            If you choose to countinue, that table will be deleted.
+            If you want to continue press [y], if not, press any other
+            key, then Enter:
+
+            """
+            x = str(input(string_input))
+            if x == 'y' or x == 'Y':
+                pass
+            else:
+                raise
+
+    def createTables(self):
+        '''
+        Creates 2 tables in the database.
+        1- ExperimentsTable: a row for each force volume experiment
+        with columns associated with experimental parameters of relevance.
+        It is created only if it does not exists already.
+        2- A table named as the input file with columns for the Force
+        Volume and topography data of the specific loaded experiment. If
+        a table with the same name existed, the old one is previously
+        deleted.
+        '''
+        self.checkTableExists()
+        sql_command1 = """
+        CREATE TABLE IF NOT EXISTS ExperimentsTable (
         id INTEGER,
+        ExperimentName TEXT NOT NULL UNIQUE,
+        nRows INTEGER,
+        nColumns INTEGER,
+        nRampPoints INTEGER,
+        mapLength REAL,
+        rampLength REAL,
+        photodiodeSensitivity REAL DEFAULT 1,
+        forceConstant REAL DEFAULT 1,
+        probeRadius REAL DEFAULT 1,
+        PRIMARY KEY (id)
+        );
+        """
+        sql_command2 = f"""
+        DROP TABLE IF EXISTS {self.file_name2}
+        """
+        sql_command3 = f"""
+        CREATE TABLE IF NOT EXISTS {self.file_name2} (
+        id INTEGER,
+        ExperimentID INTEGER NOT NULL,
         NX INTEGER,
         NY INTEGER,
         ForceForward array,
         ForceBackward array,
         Height REAL,
-        PRIMARY KEY (id));
+        PRIMARY KEY (id),
+        FOREIGN KEY (ExperimentID) REFERENCES ExperimentsTable(id)
+        );
         """
-        c.executescript(sql_command)
+        self.cursor.execute(sql_command1)
+        self.cursor.execute(sql_command2)
+        self.connector.commit()
+        self.cursor.execute(sql_command3)
         self.connector.commit()
 
-    def populateFVTable(self):
+    def populateTables(self):
         '''
-        Populates the table FVData with Force Volume and topography data
+        Populates the tables ExperimentsTable and the one named as
+        the input file.
+        If an entry with a similar ExperimentName already exists in
+        ExperimentsTable, thant entry is replaced.
         '''
+        sql_command = """
+        INSERT OR REPLACE INTO ExperimentsTable
+        (ExperimentName, nRows, nColumns,
+        nRampPoints, mapLength, rampLength) values
+        (?, ?, ?, ?, ?, ?)
+        """
+        self.cursor.execute(sql_command, (
+            self.file_name2,
+            self.numberOfMapRows,
+            self.numberOfMapColumns,
+            self.numberOfRampPoints,
+            self.mapLength,
+            self.rampLength
+        ))
+
+        sql_command2 = f"""
+        SELECT id FROM ExperimentsTable
+        WHERE ExperimentName='{self.file_name2}';
+        """
+        self.cursor.execute(sql_command2)
+
+        ExperimentID = self.cursor.fetchone()[0]
+
         for i in range(self.numberOfMapRows):
             for j in range(self.numberOfMapColumns):
                 self.cursor.execute(
-                        """INSERT INTO FVData 
-                           (NX, NY, ForceForward, ForceBackward, Height) 
-                           values (?, ?, ?, ?, ?)""",
-                        (i, j, self.FVDataArray[i, j, 0, :],
+                        f"""INSERT INTO {self.file_name2}
+                           (ExperimentID, NX, NY,
+                           ForceForward, ForceBackward, Height)
+                           values (?, ?, ?, ?, ?, ?)""",
+                        (ExperimentID, i, j, self.FVDataArray[i, j, 0, :],
                          self.FVDataArray[i, j, 1, :],
                          self.topographyArray[i, j])
                         )
         self.connector.commit()
+
+    def testFunction(self):
+        '''
+        General method for testing during development
+        '''
+        sql_command = f"""
+                      SELECT ForceForward, ForceBackward, Height
+                      FROM {self.file_name2}
+                      WHERE id = 1;
+                      """
+        self.cursor.execute(sql_command)
+        data = self.cursor.fetchall()
+        for d in data[0]:
+            print(d)
+            plt.plot(d)
+            plt.show()
 
 
 ###############################################################################
 # Run if this is the main program
 ###############################################################################
 if __name__ == "__main__":
+    # Load parsed input force volume file and output database
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--input", required=True,
                     help="path to input force volume file")
     ap.add_argument("-o", "--output", required=True,
                     help="path to output database")
     args = vars(ap.parse_args())
+
+    # Register adapter and converter for
+    # sqlite3 numpy array to bytes conversions
     sqlite3.register_adapter(np.ndarray, adapt_array)
     sqlite3.register_converter("array", convert_array)
-    NanoscopeForceVolumeFileToDataBase(args['input'], args['output'])
+
+    # Create an object of the NanoscopeForceVolumeFileToDataBase class
+    fvObject = NanoscopeForceVolumeFileToDataBase(
+        args['input'],
+        args['output']
+    )
+
+    # Reads the header of file_name, and populates
+    # the headerParameters dictionary
+    fvObject.readHeader()
+
+    # Finds meaningful parameters from the key values in headerParameters
+    fvObject.headerToParameters()
+
+    # Reads Topography (binary) data from file_name
+    fvObject.readTopography()
+
+    # Reads Force Volume (FV) data from file_name
+    fvObject.readFV()
+
+    # Connects to the database
+    fvObject.connectToDataBase()
+
+    # Creates a table in the database
+    fvObject.createTables()
+
+    # Inserts FV and Tpography data into the table
+    fvObject.populateTables()
+
+    # Closes the connection to the database
+    fvObject.closeDataBaseConnection()
